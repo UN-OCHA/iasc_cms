c5e55b9242211203b712b09ff65943518c688d74
Mai Irie <mai.irie@gmail.com>
2014-09-10 15:48:16 -0400

    Added coder
    Also adjusted the Gruntconfig.json file to include the options required for setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,25 +2,4 @@ module.exports = function(grunt) {
 
   // Load all plugins and tasks defined by the drupal-grunt-starter package.
   require('drupal-grunt-starter/Gruntfile')(grunt);
-
-  // Load all local tasks.
-  //grunt.loadTasks(__dirname + '/tasks');
-
-  //grunt.loadNpmTasks("grunt-extend-config");
-  //grunt.loadNpmTasks('grunt-wiredep');
-//
-//  var tasksDefault = [
-//    'composer:install',
-//    'newer:drushmake:default',
-//    'symlink:profiles',
-//    'symlink:modules',
-//    'symlink:themes',
-//    'clean:sites',
-//    'symlink:sites',
-//    'mkdir:files',
-//    'copy:static'
-//  ];
-
-  // Re-register the default task with a modified stack.
-//  grunt.registerTask('default', tasksDefault);
 };
