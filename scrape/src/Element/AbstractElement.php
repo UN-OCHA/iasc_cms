c3d53cc2f56fb621939df936006a8bf2e8260944
Mai Irie <mai.irie@gmail.com>
2014-09-24 16:57:08 -0400

    Changed getDOMValue() to accept array.
    $param = array(
      'get_text' => T/F <- should we get the text for the node?
      'selector' => string <- will be passed to filter() so the format is what the CssSelector expects
      'attr' => string <- default is value
    );
    
    So the only thing that's required is selector (there's no error handling here). attr and get_text are optional.
    
    Added an example setup in ContactElement->setValues(). In this example, most of the elements just use the value attribute, but the final element (the text for the selected option) uses text().

--- scrape/src/Element/AbstractElement.php
+++ scrape/src/Element/AbstractElement.php
@@ -30,10 +30,30 @@ abstract class AbstractElement implements ElementInterface
     return $output;
   }
 
-  public function getDOMValue($id) {
-    $value = $this->listing->crawler
-      ->filter($id)
-      ->attr('value');
+  /**
+   * Grabs the desired string value from the DOM element.
+   *
+   * @param $params
+   *  An array that describes what we want to grab from the DOM element.
+   * @return string
+   *  The desired string value from the dom
+   */
+  public function getDOMValue($params) {
+    // If get_text is set to TRUE, then we should use the text of the element
+    // instead of the attribute
+    if (!empty($params['get_text'])) {
+      $value = $this->listing->crawler
+        ->filter($params['selector'])
+        ->text();
+    }
+    else {
+      // If the attribute isn't specified, then we assume that we need to grab
+      // the string in the value attribute.
+      $attr = !empty($params['attr']) ? $params['attr'] : 'value';
+      $value = $this->listing->crawler
+        ->filter($params['selector'])
+        ->attr($attr);
+    }
 
     return $value;
   }
