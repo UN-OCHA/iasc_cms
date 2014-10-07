d20831a3433efd1bd94e01ccd3c3355b20cfa938
fmitchell <fmitchell@phase2technology.com>
2014-10-07 04:59:42 -0500

    IASC-7: meeting element, listing

---
+++ src/modules/iasc_scrape/IASC/src/Element/MeetingElement.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * @file
+ * Scrape Meeting element.
+ */
+
+namespace IASC\Element;
+
+use IASC\Listing\MeetingListing;
+use Goutte\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+class MeetingElement extends AbstractElement {
+  /**
+   * Constructor.
+   */
+  public function __construct(Client $client, Crawler $crawler, $page = 1, $position = 1) {
+    $this->position = $position;
+    $this->page = $page;
+    // The table id needed to use the pager. Not the id of the table element.
+    $table_id = 'ctl00$ContentPlaceHolder1$ctl00$gvmeetings';
+    $link_name = 'Meetings';
+    $listing = new MeetingListing($client, $crawler);
+    $this->goThroughListingPage($listing, $link_name, $table_id);
+    $this->setValues();
+  }
+
+  /**
+   * Set the values from HTML.
+   */
+  public function setValues() {
+    $values = array(
+      'field_authoring_bodies' => array(
+        'selector' => '#ctl00_ContentPlaceHolder1_ctl00_ddlBody option[selected="selected"]',
+        'get_text' => TRUE,
+      ),
+      'title' => array('selector' => '#ctl00_ContentPlaceHolder1_ctl00_txtTitle'),
+      'field_oa_date' => array('selector' => '#ctl00_ContentPlaceHolder1_ctl00_txtStartDate'),
+      'field_time_zone' => array('selector' => '#ctl00_ContentPlaceHolder1_ctl00_ddlZone'),
+      'field_oa_address' => array('selector' => '#ctl00_ContentPlaceHolder1_ctl00_txtCity'),
+      'field_host' => array(
+        'selector' => '#ctl00_ContentPlaceHolder1_ctl00_ddlhost option[selected="selected"]',
+        'get_text' => TRUE,
+      ),
+      'body' => array('selector' => '#ctl00_ContentPlaceHolder1_ctl00_txtpublic'),
+      'field_info_private' => array('selector' => '#ctl00_ContentPlaceHolder1_ctl00_txtprivate'),
+      'og_group_ref' => array(
+        'selector' => '#ctl00_ContentPlaceHolder1_ctl00_ddlpublev2 option[selected="selected"]',
+        'get_text' => TRUE,
+      ),
+      'field_contact' => array('selector' => '#ctl00_ContentPlaceHolder1_ctl00_SearchContacts1_hdnContactId'),
+      'field_meeting_agendas' => array(
+        'selector' => '#ctl00_ContentPlaceHolder1_ctl00_gvAgendas',
+        'table_link' => TRUE,
+        'tr' => 1,
+        'td' => 5,
+      ),
+    );
+    $this->values = $values;
+  }
+}
---
+++ src/modules/iasc_scrape/IASC/src/Listing/MeetingListing.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace IASC\Listing;
+
+use Goutte\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+class MeetingListing extends AbstractListing {
+  /**
+   * Constructor.
+   */
+  public function __construct(Client $client, Crawler $crawler) {
+    $this->client = $client;
+    $this->crawler = $crawler;
+  }
+
+  /**
+   * The edit link.
+   */
+  public function clickEditLink($position = 1) {
+    $link = $this->crawler
+      ->filter('#ctl00_ContentPlaceHolder1_ctl00_gvmeetings')
+      ->filter('tr')->eq($position)
+      ->filter('td')->eq(7)
+      ->selectLink('Edit')
+      ->link();
+
+    $this->crawler = $this->client->click($link);
+  }
+}
