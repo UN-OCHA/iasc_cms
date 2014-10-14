a3dd1b6eb5985f9e2b66bba8b8f38a0b9f114afd
Denis Babineau <denis@rockandload.ca>
2014-10-14 17:00:04 -0300

    IASC-87 fix Drupal.behaviors name

--- src/themes/iasc/assets/javascripts/script.js
+++ src/themes/iasc/assets/javascripts/script.js
@@ -3,27 +3,22 @@
  * Custom scripts for theme.
  */
 (function ($) {
-  Drupal.behaviors.rock_theme = {
+  Drupal.behaviors.iasc = {
     attach: function (context, settings) {
       
-    // Search Form Expand on Hover
-    $('.navbar-form .search-query').hover(function(){
-      $(this).addClass('grow');
-    },function(){
-      if(!$(this).is(":focus")) {
+      // Search Form Expand on Hover
+      $('.navbar-form .search-query').hover(function(){
+        $(this).addClass('grow');
+      },function(){
+        if(!$(this).is(":focus")) {
+          $(this).removeClass('grow');
+        }
+      });
+
+      $('.navbar-form .search-query').focusout(function(){
         $(this).removeClass('grow');
-      }
-    });
-
-    $('.navbar-form .search-query').focusout(function(){
-      $(this).removeClass('grow');
-    })
-
-
-
-
-
-
+      })
+      
     }
   };
 })(jQuery);
