658f8a082d9c3aef1a4c02a82d17b4722ff08ba7
Mai Irie <mai.irie@gmail.com>
2014-10-15 15:01:43 -0400

    IASC-60: coding standard fixes
    also removed debugging code.

--- src/themes/iasc/template.php
+++ src/themes/iasc/template.php
@@ -128,13 +128,13 @@ function iasc_pager($variables) {
   // Calculate various markers within this pager piece:
   // Middle is used to "center" pages around the current page.
   $pager_middle = ceil($quantity / 2);
-  // current is the page we are currently paged to
+  // Current is the page we are currently paged to.
   $pager_current = $pager_page_array[$element] + 1;
-  // first is the first page listed by this pager piece (re quantity)
+  // First is the first page listed by this pager piece (re quantity).
   $pager_first = $pager_current - $pager_middle + 1;
-  // last is the last page listed by this pager piece (re quantity)
+  // Last is the last page listed by this pager piece (re quantity).
   $pager_last = $pager_current + $quantity - $pager_middle;
-  // max is the maximum page number
+  // Max is the maximum page number.
   $pager_max = $pager_total[$element];
   // End of marker calculations.
 
@@ -152,10 +152,34 @@ function iasc_pager($variables) {
   }
   // End of generation loop preparation.
 
-  $li_first = theme('pager_first', array('text' => (isset($tags[0]) ? $tags[0] : t('« first')), 'element' => $element, 'parameters' => $parameters));
-  $li_previous = theme('pager_previous', array('text' => (isset($tags[1]) ? $tags[1] : t('‹ previous')), 'element' => $element, 'interval' => 1, 'parameters' => $parameters));
-  $li_next = theme('pager_next', array('text' => (isset($tags[3]) ? $tags[3] : t('next ›')), 'element' => $element, 'interval' => 1, 'parameters' => $parameters));
-  $li_last = theme('pager_last', array('text' => (isset($tags[4]) ? $tags[4] : t('last »')), 'element' => $element, 'parameters' => $parameters));
+  $li_first = theme('pager_first',
+    array(
+      'text' => (isset($tags[0]) ? $tags[0] : t('« first')),
+      'element' => $element,
+      'parameters' => $parameters,
+    )
+  );
+  $li_previous = theme('pager_previous',
+    array(
+      'text' => (isset($tags[1]) ? $tags[1] : t('‹ previous')),
+      'element' => $element, 'interval' => 1,
+      'parameters' => $parameters,
+    )
+  );
+  $li_next = theme('pager_next',
+    array(
+      'text' => (isset($tags[3]) ? $tags[3] : t('next ›')),
+      'element' => $element, 'interval' => 1,
+      'parameters' => $parameters,
+    )
+  );
+  $li_last = theme('pager_last',
+    array(
+      'text' => (isset($tags[4]) ? $tags[4] : t('last »')),
+      'element' => $element,
+      'parameters' => $parameters,
+    )
+  );
 
   if ($pager_total[$element] > 1) {
     if ($li_first) {
@@ -178,7 +202,14 @@ function iasc_pager($variables) {
         if ($i < $pager_current) {
           $items[] = array(
             'class' => array('pager-item'),
-            'data' => theme('pager_previous', array('text' => $i, 'element' => $element, 'interval' => ($pager_current - $i), 'parameters' => $parameters)),
+            'data' => theme('pager_previous',
+              array(
+                'text' => $i,
+                'element' => $element,
+                'interval' => ($pager_current - $i),
+                'parameters' => $parameters,
+              )
+            ),
           );
         }
         if ($i == $pager_current) {
@@ -190,7 +221,14 @@ function iasc_pager($variables) {
         if ($i > $pager_current) {
           $items[] = array(
             'class' => array('pager-item'),
-            'data' => theme('pager_next', array('text' => $i, 'element' => $element, 'interval' => ($i - $pager_current), 'parameters' => $parameters)),
+            'data' => theme('pager_next',
+              array(
+                'text' => $i,
+                'element' => $element,
+                'interval' => ($i - $pager_current),
+                'parameters' => $parameters,
+              )
+            ),
           );
         }
       }
