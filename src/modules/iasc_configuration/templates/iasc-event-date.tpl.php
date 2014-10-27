af670e057ddf717d6778cfa3f9697397d14c1c76
Mai Irie <mai.irie@gmail.com>
2014-10-27 17:23:37 -0400

    IASC-69: Add year to oa_events_date_formatter

---
+++ src/modules/iasc_configuration/templates/iasc-event-date.tpl.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ * Default template for event dates.
+ */
+?>
+<div><?php print $year; ?></div>
+
+<div class='oa-event-date-wrapper'>
+  <div class='oa-event-date-month-wrapper'>
+    <span class='oa-event-date-month'>
+      <?php print $month; ?>
+    </span>
+  </div>
+  <div class='oa-event-date-day-wrapper'>
+    <span class='oa-event-date-day'>
+      <?php print $day; ?>
+    </span>
+  </div>
+</div>
