08a826e013a26f4973090013fe5afe123d311b10
Mai Irie <mai.irie@gmail.com>
2014-11-04 12:08:04 -0500

    IASC-164: Added title existence check
    Titles are not required for panel widgets.

--- src/themes/iasc/panels/styles/secondary/secondary.tpl.php
+++ src/themes/iasc/panels/styles/secondary/secondary.tpl.php
@@ -5,9 +5,11 @@
  */
 ?>
 <div class="<?php print $class; ?>">
+  <?php if (!empty($content->title)): ?>
   <header class="panel-heading">
     <h2 class="panel-title"><?php print $content->title; ?></h2>
   </header>
+  <?php endif; ?>
   <div class="panel-body">
     <?php print render($content->content); ?>
   </div>
