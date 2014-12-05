a062723360060bebe7bbd93aa1da8a87e5400bc1
Denis Babineau <denis@rockandload.ca>
2014-12-05 08:36:49 -0400

    IASC-283 styles for the new design
    Former-commit-id: c0712b4d77c689ce747085525bb1a1738bdca13f

--- src/themes/iasc/assets/javascripts/script.js
+++ src/themes/iasc/assets/javascripts/script.js
@@ -5,21 +5,6 @@
 (function ($) {
   Drupal.behaviors.iasc = {
     attach: function (context, settings) {
-
-      // Search Form Expand on Hover
-      if(Modernizr.csstransitions) {
-        $('.navbar-form').hover(function(){
-          $(this).addClass('grow');
-        },function(){
-          if(!$('.navbar-form .search-query').is(":focus")) {
-            $(this).removeClass('grow');
-          }
-        });
-
-        $('.navbar-form .search-query').focusout(function(){
-          $('.navbar-form').removeClass('grow');
-        })
-      }
       
     }
   };
