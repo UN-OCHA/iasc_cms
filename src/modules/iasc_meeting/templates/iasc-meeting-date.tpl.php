cf585fe001403e9a4f4aaae331aebe2d62339325
Denis Babineau <denis@rockandload.ca>
2014-12-22 19:15:59 -0400

    IASC-311 Added the end month, day and year of an event
    Former-commit-id: 04a74ebd3e0c9ea21d5c6322b83ac9387f3d4b7c

--- src/modules/iasc_meeting/templates/iasc-meeting-date.tpl.php
+++ src/modules/iasc_meeting/templates/iasc-meeting-date.tpl.php
@@ -7,17 +7,29 @@
 <div class='iasc-event-date-wrapper radius'>
   <div class='iasc-event-date-month-wrapper'>
     <span class='iasc-event-date-month text-uppercase'>
-      <?php print $variables['month']?>
+      <?php if($variables['month_end'] != $variables['month']) {
+        print $variables['month'] . '-' . $variables['month_end'];
+      }else{
+        print $variables['month'];
+      } ?>
     </span>
   </div>
   <div class='iasc-event-date-day-wrapper'>
     <span class='iasc-event-date-day'>
-      <?php print $variables['day']?>
+      <?php if($variables['day_end'] != $variables['day']) {
+        print $variables['day'] . '-' . $variables['day_end'];
+      }else{
+        print $variables['day'];
+      } ?>
     </span>
   </div>
   <div class='iasc-event-date-year-wrapper'>
     <small class='iasc-event-date-year'>
-      <?php print $year; ?>
+      <?php if($variables['year_end'] != $variables['year']) {
+        print $variables['year'] . '-' . $variables['year_end'];
+      }else{
+        print $variables['year'];
+      } ?>
     </small>
   </div>
 </div>
