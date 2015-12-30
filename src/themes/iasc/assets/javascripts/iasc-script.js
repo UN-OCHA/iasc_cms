a6033be9940e1dd95bcd983e27cc93a6e7c4592a
Guillaume Viguier <guillaume@viguierjust.com>
2015-12-30 18:11:57 +0100

    Fixed issue with dropdown menu
    Former-commit-id: 4f62fd9d6f00f724c3a162e5a0197e2648493b1c

--- src/themes/iasc/assets/javascripts/iasc-script.js
+++ src/themes/iasc/assets/javascripts/iasc-script.js
@@ -12,6 +12,35 @@
         $(this).addClass('open');
       });
 
+      $('.dropdown').removeOnce('radix-dropdown');
+      $('.dropdown').once('radix-dropdown', function(){
+        var dropdown = this;
+
+        function show() {
+          if (!$(dropdown).hasClass('open')) {
+            $('>[data-toggle="dropdown"]', dropdown).trigger('click.bs.dropdown');
+            $(dropdown).addClass('open');
+          }
+        }
+
+        function hide() {
+          if ($(dropdown).hasClass('open')) {
+            $('>[data-toggle="dropdown"]', dropdown).trigger('click.bs.dropdown');
+            $(dropdown).removeClass('open');
+          }
+        }
+
+        $(this).on('mouseenter.radix.dropdown', function(e) {
+          show();
+        });
+        $(this).on('mouseleave.radix.dropdown', function() {
+          hide();
+        });
+        $(this).on('focusin.radix.dropdown', function() {
+          show();
+        });
+      });
+
     }
   };
 })(jQuery);
