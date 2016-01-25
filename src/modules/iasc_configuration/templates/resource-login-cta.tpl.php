0dfe763fd3bf6e3dc374485b9d19b97dbf197ac3
Guillaume Viguier <guillaume@viguierjust.com>
2016-01-25 15:20:56 +0100

    IASC-448: more text changes
    Former-commit-id: 529a3a01f97e7d2cfd374ef9fe76a7d616b542da

--- src/modules/iasc_configuration/templates/resource-login-cta.tpl.php
+++ src/modules/iasc_configuration/templates/resource-login-cta.tpl.php
@@ -7,8 +7,7 @@
 <div class="alert alert-warning alert-dismissible fade in" role="alert">
   <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
   <h4>Please Log In for Full Results</h4>
-  <p>Members of the IASC community should log in using their
-    <?php print l(t('group'), 'user/ac', array('query' => array('destination' => $dest))); ?>, or
-    <?php print l(t('personal accounts'), 'user/iasc_login', array('query' => array('destination' => $dest))); ?>
+  <p>Please 
+    <?php print l(t('log in'), 'user/iasc_login', array('query' => array('destination' => $dest))); ?>
     to see all available documents.</p>
 </div>
--- src/modules/iasc_meeting/templates/meeting-login-cta.tpl.php
+++ src/modules/iasc_meeting/templates/meeting-login-cta.tpl.php
@@ -7,8 +7,7 @@
 <div class="alert alert-warning alert-dismissible fade in" role="alert">
   <button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>
   <h4>Please Log In for Full Document Listing</h4>
-  <p>Meetings often reference non-public documents. Members of the IASC community should log in using their
-    <?php print l(t('group'), 'user/ac', array('query' => array('destination' => $dest))); ?>, or
-    <?php print l(t('personal accounts'), 'user/iasc_login', array('query' => array('destination' => $dest))); ?>
+  <p>Meetings often reference non-public documents. Please 
+    <?php print l(t('log in'), 'user/iasc_login', array('query' => array('destination' => $dest))); ?>
     to see all available content.</p>
 </div>
