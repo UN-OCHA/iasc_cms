34774b469cb5d32256e85c944b3859f6e3a0e9f4
Guillaume Viguier <guillaume@viguierjust.com>
2017-05-05 14:49:26 +0200

    IASC-500 - set gc_maxlifetime to 1 minute for testing

--- src/sites/default/settings.php
+++ src/sites/default/settings.php
@@ -293,7 +293,7 @@ ini_set('session.gc_divisor', 100);
  * a session is deleted, authenticated users are logged out, and the contents
  * of the user's $_SESSION variable is discarded.
  */
-ini_set('session.gc_maxlifetime', 200000);
+ini_set('session.gc_maxlifetime', 60);
 
 /**
  * Set session cookie lifetime (in seconds), i.e. the time from the session is
